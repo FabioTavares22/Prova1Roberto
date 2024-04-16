e2f66f391959f820813bda57ff51ca604a13994f
Fabio Tavares <fabiogardine@gmail.com>
2024-04-16 19:18:18 -0300

    Commit inicial

---
+++ Item-7/src/main/java/com/example/Item7/Item7Application.java
@@ -0,0 +1,13 @@
+package com.example.Item7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Item7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Item7Application.class, args);
+	}
+
+}
---
+++ Item-7/src/test/java/com/example/Item7/Item7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Item7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Item7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
